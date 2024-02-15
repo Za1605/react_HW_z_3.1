c756a09e180bac9d7e9d0a69c5c724d427b928a5
YuliiaT <115080417+Za1605@users.noreply.github.com>
2024-02-15 22:53:23 +0200

    3.2

--- src/components/CarsContainer/CarsForm.js
+++ src/components/CarsContainer/CarsForm.js
@@ -1,17 +1,23 @@
 import React from 'react';
+import {useForm} from "react-hook-form";
+import {options} from "axios";
 
 const CarsForm = () => {
-    const {register, reset, handleSubmit, formState :{isValid, errors}, setValue}
+    const {register,reset, handleSubmit, formState :{isValid, errors}, setValue}= useForm
+    ({mode:'all'});
 
 
 
          const save =(car) =>{
 
          }
-    return ( <form onSubmit={save}>
-            <input type={"text"} placeholder={'brand'} {...register('name')}/>
-             <input type={}/>
-
+    return ( <form onSubmit={handleSubmit(save)}>
+            <input type={"text"} placeholder={'brand'} {...register('brand',
+                {pattern:/[a-zA-Zа-яА-яёЁіІїЇєЄҐґ]{1,20}$/})}/>
+             <input type={"text"} placeholder={'price'} {...register('price', {valueAsNumber: true}
+             />
+            <input type={"text"} placeholder={'year'}{...register('year', {valueAsNumber:true})}/>
+        <button>Save</button>
         </form>
 
          );
